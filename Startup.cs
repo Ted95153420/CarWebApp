b4060b07faa040660d3475553a4d9771d0f59a24
Ted95153420 <edward.milne@gmail.com>
2018-09-27 05:47:18 -0700

    added a change to test github

--- Startup.cs
+++ Startup.cs
@@ -38,6 +38,7 @@ namespace CarPriceComparison
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            //testing comment
             app.UseDefaultFiles();
             app.UseStaticFiles();
          }
