0626ba7486e1639b9ad4b0b1f2423e13563c89cd
Ted95153420 <edward.milne@gmail.com>
2018-10-02 05:49:35 -0700

    modified git ignore. added simple javascript

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//Chapter 3 - show a quick demo of Javascript in action
+
+//Chapter 3 - talk about immediately invokable function and why we implement this
+//hint : so there are no variable 'clashes' in the global scope
+(function() {
+    var element = document.getElementById("welcomelabel");
+    element.innerHTML = "Welcome Ted! ";
+})();
