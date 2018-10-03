274e4faa991cd54c3c94d2196e9bcfd541d57953
Ted95153420 <edward.milne@gmail.com>
2018-10-03 05:54:54 -0700

    enabled MVC

--- Startup.cs
+++ Startup.cs
@@ -38,9 +38,15 @@ namespace CarPriceComparison
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            //Chapter 3 - note Use Default and Use Static
-            app.UseDefaultFiles();
             app.UseStaticFiles();
+
+            app.UseMvc( config =>{
+                config.MapRoute(
+                    name    : "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new {controller = "Home", action = "Index"}
+                );
+            });
          }
     }
 }
