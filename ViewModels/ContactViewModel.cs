74ea0562fdd386533fb7325171cb6f9b39c5d615
Ted95153420 <edward.milne@gmail.com>
2018-11-03 07:49:11 -0700

    bound Contact to ContactViewModel

---
+++ ViewModels/ContactViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CarPriceComparison.ViewModels
+{
+    public class ContactViewModel
+    {
+        public string Name { get; set; }
+        public string EMail { get; set; }
+        public string Message { get; set; }
+    }
+}
