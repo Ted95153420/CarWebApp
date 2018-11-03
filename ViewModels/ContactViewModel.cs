4ea6ca39f9f8bffc58ea984eaa7b6daf31cb91ec
Ted95153420 <edward.milne@gmail.com>
2018-11-03 08:12:33 -0700

    added basic validation to contact form

--- ViewModels/ContactViewModel.cs
+++ ViewModels/ContactViewModel.cs
@@ -1,11 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarPriceComparison.ViewModels
 {
     public class ContactViewModel
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
         public string EMail { get; set; }
+        [Required]
+        [StringLength(4096, MinimumLength =10)]
         public string Message { get; set; }
     }
 }
