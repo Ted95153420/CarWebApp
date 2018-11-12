ffade282323a85af66bac4433221e558b8e22abb
Ted95153420 <edward.milne@gmail.com>
2018-11-12 07:11:54 -0800

    bootstrap 4 navbar added (basic)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -8,6 +8,10 @@
 //Chapter 3 - talk about immediately invokable function and why we implement this
 //hint : so there are no variable 'clashes' in the global scope
 (function() {
-    var element = document.getElementById("usernameheader");
-    element.innerHTML = "Welcome Ted! ";
+    //var element = document.getElementById("usernameheader");
+    //element.innerHTML = "Welcome Ted! ";
+
+    document.addEventListener("click", function(){
+        document.getElementById("usernameheader").innerHTML = "Hello World!";
+    });
 })();
