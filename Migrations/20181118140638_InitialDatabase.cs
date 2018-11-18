2251aa414efa9ea271790c81defb25571e825d67
Ted95153420 <edward.milne@gmail.com>
2018-11-18 06:07:50 -0800

    added migrations so database can be created

---
+++ Migrations/20181118140638_InitialDatabase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarPriceComparison.Migrations
+{
+    public partial class InitialDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VehicleMakes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Make = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehicleMakes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VehicleMakes");
+        }
+    }
+}
