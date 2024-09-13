9b7c0358f3eb0433dfcede6840239840976368d0
Ted Milne <EDMilne@bclc.com>
2024-09-13 09:49:10 -0700

    able to make entrypoint.sh output messages to the log file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,21 @@
+echo "Made it to entrypoint.sh" >> /proc/1/fd/1
 #!/bin/bash
 # entrypoint.sh
 
+echo "Entry point script started..." >> /proc/1/fd/1
+
 # Start SQL Server
+echo "Starting SQL Server..." >> /proc/1/fd/1
 /opt/mssql/bin/sqlservr &
 
 # Wait for SQL Server to start
+echo "Waiting for SQL Server to start..." >> /proc/1/fd/1
 sleep 30s
 
 # Run Entity Framework migrations
-dotnet ef database update --project /app/CarPriceComparison.csproj
+echo "Running Entity Framework migrations..." >> /proc/1/fd/1
+dotnet ef database update --project CarPriceComparison.csproj
+echo "Running Entity Framework migrations..." >> /proc/1/fd/1
 
 # Run the setup script
 /opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -d master -i /usr/src/app/setup.sql -C
